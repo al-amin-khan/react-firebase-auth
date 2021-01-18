4f14ba3a7dbfa53c6bccea2722b5e78d275efd70
al-amin-khan <alaminkhan2010@gmail.com>
2021-01-18 22:06:57 +0600

    google sign in method added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [signedIn, setSignedIn] = useState({
+    isSignIn: false,
+    displayName : '',
+    email: '',
+    photoURL: '',
+  });
+
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  // firebase.auth().useDeviceLanguage();
+
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+      const credential = result.credential;
+      const token = credential.accessToken;
+      const user = result.user;
+      const {displayName, email, photoURL} = user; 
+      
+      setSignedIn({
+        isSignIn: true,
+        displayName: displayName,
+        email: email,
+        photoURL: photoURL,
+      });
+    })
+    .catch(error => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+    })
+
+  };
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(res => {
+      const logout = {
+        isSignIn: false,
+        displayName: '',
+        email: '',
+        photoURL: '',
+      }
+      setSignedIn(logout);
+    })
+    .catch(error => console.log(error))
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        signedIn.isSignIn ? 
+        <button onClick={handleSignOut}>Logout</button>
+        :
+        <button onClick={handleSignIn}>Sign-in with Google</button>
+      }
+      {
+        signedIn.isSignIn ?
+      <div>
+      <h2>Welcome, {signedIn.displayName}</h2>
+      <p>Email: {signedIn.email}</p>
+      <img src={signedIn.photoURL} alt={signedIn.displayName}/>
+      </div>
+      :
+      <h2>You are not logged in.</h2>
+      }
+
     </div>
   );
 }
